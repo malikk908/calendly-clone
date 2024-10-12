5fe7459522f4ce7c7dacf31c42c6f50522082e82
malikk908 <malik.usama5810@gmail.com>
2024-10-12 19:08:51 +0500

    clerk setup

---
+++ src/app/((private))/events/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs"
+
+export default function EventsPage() {
+
+    return <>
+        <UserButton />
+        <h1>Events</h1>
+    </>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return null
+  
+  const { userId } = auth();
+  if (userId != null) redirect("/events");
+
+  return <div className="text-center container mx-auto my-4">
+    <h1 className="text-3xl mb-4">
+      Home Page
+    </h1>
+    <div className="flex gap-2 justify-center">
+      <Button asChild>
+        <SignInButton />
+      </Button>
+      <Button asChild>
+        <SignUpButton />
+      </Button>
+      <UserButton/>
+    </div>
+  </div>
 }
